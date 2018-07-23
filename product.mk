2116cde53a00a97abe1b1b776326e2edd3d3edc1
Danny Baumann <dannybaumann@web.de>
2018-07-23 10:03:43 +0200

    Properly filter device name.
    Change-Id: Ibfb706769b253dfebc4a3711161ede91dce1342a

--- product.mk
+++ product.mk
@@ -1,6 +1,9 @@
 # Google Apps
+
+DEVICE := $(shell echo $(LINEAGE_BUILD) | sed 's/^.*-//')
+
 ifeq ($(WITH_GMS), true)
-ifeq ($(filter lineage_r5,$(PRODUCT_NAME)),)
+ifeq ($(DEVICE),r5)
 $(call inherit-product, vendor/gapps/arm/arm-vendor.mk)
 else
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
