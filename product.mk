75e0114f328da32650d6cd4e83b0cc873360fbd2
Danny Baumann <dannybaumann@web.de>
2018-07-23 10:03:56 +0200

    Include OnePlus camera on OP6.
    Change-Id: I09e922c783832cef7cc740034bdc0251f8b9ed60

--- product.mk
+++ product.mk
@@ -10,6 +10,10 @@ $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 endif
 
+ifeq ($(DEVICE),enchilada)
+$(call inherit-product, vendor/onepluscam/oneplus-camera.mk)
+endif
+
 # Recovery ADB keys
 PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:recovery/root/adb_keys
