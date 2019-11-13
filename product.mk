b793ff18013aa13d4a3580d3e90d78eaf23b4d3c
Danny Baumann <dannybaumann@web.de>
2019-11-13 10:40:06 +0100

    Stop building OnePlus camera.
    Using Gcam now.
    
    Change-Id: Ia0a33c3249559f6b8ad75f78f724f79b9bf76d74

--- product.mk
+++ product.mk
@@ -7,9 +7,6 @@ GAPPS_VARIANT := nano
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 endif
 
-ifeq ($(DEVICE),enchilada)
-$(call inherit-product, vendor/onepluscam/oneplus-camera.mk)
-endif
 
 # Recovery ADB keys
 PRODUCT_COPY_FILES += \
